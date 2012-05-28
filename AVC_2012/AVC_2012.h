d63c76432e3a26885989716d2fcbeaf2236ab2d4
richard@burnsides.org <richard@burnsides.org@d86039bd-945b-f367-b13c-a3cd50f3cf67>
2012-05-28 21:39:21 +0000

    - adjust RR gyro call number - created variable call "STEER_ADJUST" because this will be different for each vehicle - added variable to header and main code called "WAYPOINT_ACCEPT" since this may be different for each vehicle
    git-svn-id: https://minuteman.googlecode.com/svn/trunk@45 d86039bd-945b-f367-b13c-a3cd50f3cf67

--- AVC_2012/AVC_2012.h
+++ AVC_2012/AVC_2012.h
@@ -4,11 +4,13 @@
 #define TIRE_CAL 1.5		//tire calibration factor. ***THIS IS JUST A PLACE HOLDER FOR NOW***
 #define STEER_ADJUST 1425	//steering adjustment factor. ***THIS IS JUST A PLACE HOLDER FOR NOW***
 #define CAR_NAME ***MINUTEMAN*** //car name
+#define WAYPOINT_ACCEPT 25	//waypoint acceptance radius
 #endif
 
 #ifdef RR
-#define GYRO_CAL 8650000	//this has to be measured by rotating the gyro 360 deg. and reading the output
+#define GYRO_CAL 8700000	//this has to be measured by rotating the gyro 360 deg. and reading the output
 #define TIRE_CAL 1.5		//tire calibration factor. ***THIS IS JUST A PLACE HOLDER FOR NOW***
-#define STEER_ADJUST 1425	//steering adjustment factor. ***THIS IS JUST A PLACE HOLDER FOR NOW***
+#define STEER_ADJUST 1500	//steering adjustment factor. ***THIS IS JUST A PLACE HOLDER FOR NOW***
 #define CAR_NAME ***ROADRUNNER*** //car name
+#define WAYPOINT_ACCEPT 70	//waypoint acceptance radius
 #endif
