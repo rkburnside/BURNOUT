79063ad360fb7dc67988a0c33298f0cae126867a
richard@burnsides.org <richard@burnsides.org@d86039bd-945b-f367-b13c-a3cd50f3cf67>
2013-04-05 12:34:12 +0000

    git-svn-id: https://minuteman.googlecode.com/svn/trunk@138 d86039bd-945b-f367-b13c-a3cd50f3cf67

--- AVC_2013_Beetle/AVC_2013_Beetle.ino
+++ AVC_2013_Beetle/AVC_2013_Beetle.ino
@@ -1,56 +1,41 @@
-/* TO DO LIST
+/* 2013 Sparkfun Autonomous Vehicle Competition Code
+Vehicle: Wile E. Coyote
+Author: Richard Burnside
+Verion: 1.0
+Date: 4/4/2013
 
-x01 - add sd card initialization in setup() - column headings, new line, etc
-x02 - add time stamp to sd card data output
-x03 - clean up gps code so that i only get the information that i need
-x04 - output the gps info i need to serial, lcd, and sd card
-x05 - add a 3 second delay into the beginning of the program 'cause that is what sparkfun wants
-x06 - have the location for all the waypoints i need
-x07 - write routine that calculates the distance and direction to next waypoint
-x08 - write routine to determine when i have reached a way point
-x09 - write a routine to proceed to the next waypoint
-x10 - write to SD card once a second and combine the others into one single string
-x11 - change the variable name of SELECT to SELECT
-x12 - ensure that strobe is working correctly
-x13 - hook up steering servo
-x14 - setup servos so the radio is in control unless a switch is flipped giving the arduino control
-x15 - hook up gyro and make sure it is working correctly
-x16 - verify gryo heading is actually working correctly
-x17 - write a routine to turn right or whatever direction i need
-x18 - remove LCD and serial writing to determine many clock cycles they each take
-x19 - figure out how to set the initial conditions <- deleted...not using gyroscope
-x20 - clean up serial outputs of data <- serial mode is disabled...not needed anymore
-x21 - incorporate the throttle on the car!
-x22 - incorporate steering limits so i don't roll the car again
-x23 - mail in AVC video
-x24 - debug the shutoff function in the "waypoint" function
-x25 - add a button that will tell the arduino to start going AFTER the switch is flipped
-x26 - build a roll cage
-x27 - hot glue everything
-x28 - reattach all appropriate items with zipties
-x29 - verify that the car still stops correctly
-x30 - delete all of the unneeded GPS items that I have listed (alt, speed, course, time, date, etc)
-x31 - use an array to determine GPS points, distances and angles using arrays and get averages of them
-x32 - path forward: ignore gps heading correct between corners of building or when it is > 20-ft of waypoint
-x33 - still use the gps to determine distance
-x34 - choose waypoints a great distance to reduce possible heading change
-x35 - use ultrasonic sensors to avoid curbs
-x36 - develop a correct method/procedure for programming waypoints and driving them consistently
-x37 - clean up lcd outputs
-x38 - set up the ultrasonic sensor on an interrupt
-x39 - during the competition...make sure the final waypoint is ~20 feet beyond the finish line
+Microcontroller: Arduino nano
+Sensors to add: steering servo, speed control servo, GPS unit, bluetooth, magnetometer
+Vehicle: Turnigy Beetle RC car with 7.2V battery and 2.4-GHz TX/RX
 */
 
-/* Sparkfun Autonomous Vehicle Competition Vehicle Code
-Author: Richard Burnside
-Verion: .04
-Date: 4/15/2011
-
-Microcontroller: chinduino (arduino mega)
-Sensors to add: steering servo, speed control servo, GPS unit, SD card data logger, 40x4 LCD
-Completed Sensors: ultrasonic range finder, IR range sensor, gyroscope
-
-Vehicle: RC car with 7.2V battery and 75mhz receiver
+/* TO DO LIST
+- clean up gps code so that i only get the information that i need
+- output the gps info i need to serial and then bluetooth
+- create serial driven menu system using serial UI
+- add manual over ride switch (i.e. competition mode)
+- add ~.5 second delay after flipping switch
+- have the location for all the waypoints i need
+- incorporate cross track error handling
+- write routine that calculates the distance and direction to next waypoint
+- write routine to determine when i have reached a way point
+- write a routine to proceed to the next waypoint
+- use latitude/longitude for waypoint acceptance
+- verify copmass heading is actually working correctly
+- write a routine to turn right or whatever direction i need
+- incorporate steering limits so i don't roll the car again
+- set gain for steering (i.e. more vs. less agressive steering)
+- set gain for throttle (i.e. more vs. less acceleration)
+- mail in AVC video
+- create shut down function for when the car passes the finish line
+- create shut down or reverse function if the car hasn't moved for ~15 seconds from its current spot
+- build a roll cage
+- hot glue everything
+- zipties everything
+- delete all of the unneeded GPS items that I have listed (alt, speed, course, time, date, etc)
+- use an array to determine GPS points, distances and angles using arrays and get averages of them
+- path forward: ignore gps heading correct between corners of building or when it is > 20-ft of waypoint
+- develop a correct method/procedure for programming waypoints and driving them consistently
 */
 
 /* How servos work
@@ -62,7 +47,7 @@ ch1 -	1145	1515	1884
 ch2 -	1145	1515	1884
 ch3 -	1145	1515	1884 
 ch4 -	1145	1515	1884
-ch5 -	1130			1884 <-- push switch toward batteries is 1130, pull switch to front is 1884	
+ch5 -	1130	1515	1884 <-- push switch toward batteries is 1130, pull switch to front is 1884	
 ch6 -	1395	1510	1615
 */
 
@@ -91,14 +76,8 @@ ch6 -	1395	1510	1615
 #include <Servo.h> 
 #include <math.h>
 #include <TinyGPS.h>
-#include <LiquidCrystal.h>
 
 //Sensors & Servors
-#define TRIGGER 35				//pin # for usonic sensor trigger, P*-9
-#define TRIGGER2 37				//pin # for usonic sensor trigger, P*-9
-#define ECHO 34					//pin # for usonic sensor echo, P1
-#define ECHO2 36				//pin # for usonic sensor echo, P1
-#define IR_PIN 15				//pin # for IR range finder
 #define RECEIVER_IN 13			//pin # for receiver IN
 #define STROBE 11				//pin # for Strobe
 #define SELECT 10				//pin # for Select
@@ -109,7 +88,6 @@ Servo speed;
 bool autonomous = false;		// true is ON, false is OFF
 bool race_start = true;		// creates a 3 second delay to start the vehicle
 unsigned long temp_time = 0;	// CHANGE THIS SINCE THE SD CARD IS NOT USED ANYMORE
-int i_distance, u_distance, u_distance2;		// distances variables
 
 //Compass
 #define XCLR 8					// pin # for compass XCLR
@@ -119,26 +97,6 @@ int i_distance, u_distance, u_distance2;		// distances variables
 #define COMP_Y 2040				// compass center Y
 double compass_heading = 0;
 
-//LCD Setup
-#define RS 22
-#define ENABLE_1 24
-#define ENABLE_2 23
-#define D4 25
-#define D5 26
-#define D6 27
-#define D7 28
-LiquidCrystal lcd1(RS, ENABLE_2, D4, D5, D6, D7);
-LiquidCrystal lcd2(RS, ENABLE_1, D4, D5, D6, D7);
-String dataString = "";
-
-// Gyro Setup
-#define GYRO_CAL 8741543
-#define GYRO_LIMIT 1000			// defines how many gyro samples are taken between angle calculations
-volatile boolean new_cycle = false, cal_flag = false;
-boolean aux=1;
-volatile long gyro_sum = 0, gyro_count = 0, gyro_null=0, angle=0;
-
-//long angle_diff;				// for the gyro
 double angle_diff;				// for the compass
 
 //GPS is connected to Serial1 RX pin of arduino mega -- YELLOW/GREEN IS TX FROM THE GPS TO RX ON THE ARDUINO
@@ -155,50 +113,31 @@ int special_waypoint_num = 4; // ALWAYS 1 LESS 'cause arrays start at 0, not 1!
 const int waypoint_total = 15;	// <- should always be the same number of GPS waypoints
 // 0 = lat use, 1 = south lat use, 2 = north lat use
 double gps_array[15][3] = {{40.0651517950864528, -105.2097273131420, 0}, // 1st corner
-{40.0650036900410714, -105.2097272812302, 1}, // 1st line up point
-{40.0649066081190785, -105.2097109401257, 1}, // 2nd line up point or start of straight line
-{40.06485, -105.20975, 1}, // just added today
-{40.064528, -105.2097763511701, 1}, // lat point for start of turn
-{40.06448686920148, -105.20985212357533, 0}, // 2nd corner, just inside of south parking lot
-{40.06445080617122, -105.21016393037269, 0}, // bottom middle of south parking lot
-{40.06449104008855, -105.21041203470733, 0}, // inbetween sidewalks for 3rd corner
-{40.06450670940305, -105.2104463610966, 0}, // 3rd corner
-{40.06465758628262, -105.21046433778312, 2}, // line up for inbetween next bottleneck
-{40.06475817068338, -105.21046970220115, 2}, // past the bottleneck
-{40.06493060073908, -105.21048043103721, 2}, // middle of the 3rd side
-{40.06514305860027, -105.21048311324623, 2}, // end of 3rd side
-{40.06519745591719, -105.21041069360282, 0}, // 4th corner
-{40.06518985782108, -105.21001238556421, 0}}; // way past the finish line
+	{40.0650036900410714, -105.2097272812302, 1}, // 1st line up point
+	{40.0649066081190785, -105.2097109401257, 1}, // 2nd line up point or start of straight line
+	{40.06485, -105.20975, 1}, // just added today
+	{40.064528, -105.2097763511701, 1}, // lat point for start of turn
+	{40.06448686920148, -105.20985212357533, 0}, // 2nd corner, just inside of south parking lot
+	{40.06445080617122, -105.21016393037269, 0}, // bottom middle of south parking lot
+	{40.06449104008855, -105.21041203470733, 0}, // inbetween sidewalks for 3rd corner
+	{40.06450670940305, -105.2104463610966, 0}, // 3rd corner
+	{40.06465758628262, -105.21046433778312, 2}, // line up for inbetween next bottleneck
+	{40.06475817068338, -105.21046970220115, 2}, // past the bottleneck
+	{40.06493060073908, -105.21048043103721, 2}, // middle of the 3rd side
+	{40.06514305860027, -105.21048311324623, 2}, // end of 3rd side
+	{40.06519745591719, -105.21041069360282, 0}, // 4th corner
+	{40.06518985782108, -105.21001238556421, 0}}; // way past the finish line
 
 void setup() {
 	//Set the car to manual mode
 	autonomous = false;
-	pinMode(RECEIVER_IN, INPUT);
-	pinMode(STROBE, OUTPUT);
-	pinMode(SELECT, OUTPUT);
-	digitalWrite(STROBE, LOW);
-	digitalWrite(SELECT, HIGH);
-
 	steering.attach(SERVO);		//Servo Initialization
 	speed.attach(THROTTLE);		//Speed control initialization
 	
 	//Serial Output Initialization
 	Serial.begin(115200);
 	Serial.println("Time,IR Distance,Ultrasonic Distance, Gyro Heading, Lat, Lon, Date, Time, Course, Speed, Failed");
-	Serial1.begin(4800);		//GPS serial port initialization
 
-	//Ultrasonic Range Finder Initialization
-	pinMode(TRIGGER, OUTPUT);
-	pinMode(ECHO, INPUT);
-	digitalWrite(TRIGGER, LOW);
-	digitalWrite(ECHO, LOW);
-
-	//Ultrasonic Range 2 Finder Initialization
-	pinMode(TRIGGER2, OUTPUT);
-	pinMode(ECHO2, INPUT);
-	digitalWrite(TRIGGER2, LOW);
-	digitalWrite(ECHO2, LOW);
-	
 	//Compass Module Initialization
 	pinMode(XCLR, OUTPUT);
 	digitalWrite(XCLR, LOW);
@@ -213,55 +152,33 @@ void setup() {
 	Wire.endTransmission();					//wait for SET action to settle
 	delay(10);
 	
-	//LCD Setup
-	lcd1.begin(40, 2);
-	lcd2.begin(40, 2);
-	lcd1.clear();
-	lcd2.clear();
-	lcd1.setCursor(0, 0); // top left
-	lcd1.print("THE ARDUINO AUTONOMOUS VEHICLE PROJECT");
-	lcd2.setCursor(0, 0); // top left
-	lcd2.print("DESIGNED BY RICHARD BURNSIDE, 9763 COVE CREEK DR, HIGHLANDS, CO 80129");
-	delay(500);
-
-	lcd1.clear();
-	lcd2.clear();
-	set_gyro_adc();						// sets up free running ADC for gyro
-	lcd1.print("calculating gyro null");
 	Serial.println("gyro null calculation begin");
-	calculate_null();
-	Serial.println("gyro null calibration complete");
-	lcd2.print("calculation complete");
+
 	delay(500);
 }
 
 void loop() {
 	//GPS data update
 	bool newdata = false;
-	if(feedgps())
-		newdata = true;
-	if(newdata)
-		gpsdump(gps);
+	if(feedgps()) newdata = true;
+	if(newdata) gpsdump(gps);
 
-//	IR_sensor();    //<- FIX THIS!!!
-//	u_sonic();
 	waypoint();
 	compass();
 	set_turn();
 	set_speed();
 
-		if((millis() - temp_time) > 250) {
+	if((millis() - temp_time) > 250) {
 		auto_control();					// check for autonomous mode 4x / second
-		LCD_data_log();
-//		Serial_data_log();
+		//		Serial_data_log();
 		temp_time = millis();
 	}
 }
 
 /*----------unfinished code----------*/
 void set_speed(void) {	// test this and delete the delays after it works
-		if(waypoint_distance <= 20)		speed.write(112);
-		if(waypoint_distance > 20)		speed.write(150);
+	if(waypoint_distance <= 20)		speed.write(112);
+	if(waypoint_distance > 20)		speed.write(150);
 	return;
 }
 
@@ -293,7 +210,7 @@ void waypoint(void) {				// Distance and angle to next waypoint
 
 /*---------completed functions---------*/
 void set_turn(void) {				// Set servo to steer in the direction of the next waypoint
-double servo_angle;
+	double servo_angle;
 
 	angle_diff = compass_heading - waypoint_heading;
 
@@ -304,8 +221,8 @@ double servo_angle;
 	servo_angle = 180.0 - servo_angle;
 	
 	if(servo_angle > 130.0)		steering.write(130);
-    else if(servo_angle < 50.0)	steering.write(50);
-    else						steering.write(servo_angle);
+	else if(servo_angle < 50.0)	steering.write(50);
+	else						steering.write(servo_angle);
 	return;
 }
 
@@ -365,51 +282,10 @@ void auto_control(void) {			// Autonomous Mode
 	return;
 }
 
-void set_gyro_adc() {				// Sets Gyro auto-triggering
-	ADMUX = B01000001;					//completely reset the MUX. should be sampling only on A1, set vref to internal
-	ADCSRA = B11101111;					//set scaler, auto trigger, sampling, etc
-	Serial.println("gyro adc set");
-	return;
-}
-
-ISR(ADC_vect) {						// ADC interrupt
-	gyro_sum += ADCL | (ADCH << 8);
-	gyro_count++;        //iterate the counter
-
-	if(gyro_count == GYRO_LIMIT) {
-		angle += (gyro_sum - gyro_null);
-		if((angle > GYRO_CAL) && (!cal_flag)) 	angle -= GYRO_CAL; //if we are calculating null, don't roll-over
-		if((angle < 0) && (!cal_flag))			angle += GYRO_CAL;
-		gyro_sum = 0;
-		gyro_count = 0;
-		new_cycle = true;
-	}
-	return;
-} 
-
-void calculate_null() {				// Calculates the gyro Null
-	cal_flag = true;
-	new_cycle = false;				// this will be set, already, but need to begin on new cycle
-	while(!new_cycle) ;			// wait for start of new cycle
-	angle = 0;						// reset the angle. angle will act as accumulator for null calculation
-	gyro_null = 0;					// make sure to not subract any nulls here
-	for(int i=0; i <= 50; i++) {
-		while(!new_cycle);
-		new_cycle = false;			// start another round
-	}
-	gyro_null = angle/50;			// calculate the null
-	cal_flag = false;
-	angle = 0;
-	Serial.println("gyro null calculation complete");
-	Serial.print("gyro null: ");
-	Serial.println(gyro_null);
-	return;
-}
-
 bool feedgps() {					// GPS Data Input
 	while(Serial1.available()) {
 		if(gps.encode(Serial1.read()))
-			return true;
+		return true;
 	}
 	return false;
 }
@@ -441,8 +317,6 @@ void Serial_data_log() {			// Serial Data Logging
 	dataString += String(millis());
 	dataString += ","; dataString += String(time);
 	dataString += ","; dataString += String(gps_speed);
-	dataString += ","; dataString += String(i_distance);
-	dataString += ","; dataString += String(u_distance);
 	dataString += ","; dataString += String(gyro_null);
 	dataString += ","; dataString += String(angle);
 	dataString += ","; dataString += String(angle_string);
@@ -455,78 +329,19 @@ void Serial_data_log() {			// Serial Data Logging
 	dataString += ","; dataString += String(failed);
 
 	if(autonomous)
-		dataString += ",Auto";
+	dataString += ",Auto";
 	else
-		dataString += ",Manual";
+	dataString += ",Manual";
 
 	Serial.println(compass_heading,1);
 	Serial.println(waypoint_heading,1);
-//	servo_angle is not a global variable Serial.println(servo_angle,1);
+	//	servo_angle is not a global variable Serial.println(servo_angle,1);
 	Serial.println(angle_diff,1);
 
 	//Serial printing of string
 	Serial.println(dataString);
 	dataString = "";				//resets the datastring to 0
 	
-	return;
-}
-
-void LCD_data_log() {				// LCD Data Logging
-	lcd1.clear();
-	lcd1.setCursor(0,0);	lcd1.print("Lt/Ln: "); lcd1.print(flat,6); lcd1.print(" "); lcd1.print(flon,6);
-	lcd1.setCursor(29,0);	lcd1.print("Dft: "); lcd1.print(waypoint_distance,1);
-
-	lcd1.setCursor(0,1);	lcd1.print("NWP:   "); lcd1.print(gps_array[waypoint_num][0],6); lcd1.print(" "); lcd1.print(gps_array[waypoint_num][1],6);
-	lcd1.setCursor(29,1);	lcd1.print("Spd: "); lcd1.print(max_speed);
-	lcd1.setCursor(36,1);	lcd1.print("M: "); lcd1.print(autonomous);
-
-	lcd2.clear();
-							lcd2.print("UD_in: "); lcd2.print(u_distance2);
-	lcd2.setCursor(10,0);	lcd2.print("UD_in: "); lcd2.print(u_distance);
-	lcd2.setCursor(20,0);	lcd2.print("Tm: "); lcd2.print(millis()/1000);
-	lcd2.setCursor(28,0);	lcd2.print("Nl: "); lcd2.print(gyro_null);
-
-//	lcd2.setCursor(0,1);	lcd2.print("GY: "); lcd2.print(angle*360.0/GYRO_CAL,1);
-	lcd2.setCursor(0,1);	lcd2.print("CM: "); lcd2.print(compass_heading,1);
-	lcd2.setCursor(10,1);	lcd2.print("Hdng: "); lcd2.print(waypoint_heading,1);
-//	lcd2.setCursor(22,1);	lcd2.print("Diff: "); lcd2.print(angle_diff*360.0/GYRO_CAL,1);
-	lcd2.setCursor(22,1);	lcd2.print("Diff: "); lcd2.print(angle_diff,1);
-
-	lcd2.setCursor(33,1);	lcd2.print("WP: "); lcd2.print(waypoint_num);
-
-return;
-}
-
-void IR_sensor(void) {				//IR Distance Sensor
-	double voltage;
-	voltage = 5.0 * analogRead(IR_PIN) / 1023.0;	//reads the IR pin and returns a value between 0 & 1023
-	i_distance = 10.347244 * pow(voltage,-1.254);
-	if(i_distance > 60)	i_distance = 60;	//no object detected
-	return; 								//distance converted from float to int
-}
-
-void u_sonic(void) {				// Ultrasonic Distance Sensor
-	double duration, duration2;
-	//HIGH pulse of 10 or more microseconds is neede to TRIGGER sensor.  Give a short LOW pulse beforehand to ensure a clean HIGH pulse.  Pulse whose duration is the time (in microseconds) from the sending of the ping to the reception of its echo off of an object.
-
-	digitalWrite(TRIGGER, LOW);
-	delayMicroseconds(2);
-	digitalWrite(TRIGGER, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(TRIGGER, LOW);
-	duration = pulseIn(ECHO, HIGH);
-	u_distance = duration / 73.746 / 2.0;	//There are 73.746 microseconds per inch (i.e. sound travels at 1130 feet per second).  The distance travelled by the ping, outbound and return, so we divide by 2 to get the distance of the obstacle.  If the pulse is >36000 useconds, no object was detected
-	if(u_distance > 60)	u_distance = 60;
-
-	digitalWrite(TRIGGER2, LOW);
-	delayMicroseconds(2);
-	digitalWrite(TRIGGER2, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(TRIGGER2, LOW);
-	duration2 = pulseIn(ECHO2, HIGH);
-	u_distance2 = duration2 / 73.746 / 2.0;	//There are 73.746 microseconds per inch (i.e. sound travels at 1130 feet per second).  The distance travelled by the ping, outbound and return, so we divide by 2 to get the distance of the obstacle.  If the pulse is >36000 useconds, no object was detected
-	if(u_distance2 > 60)	u_distance2 = 60;
-
 	return;
 }
 
