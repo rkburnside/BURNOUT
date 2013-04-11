1edd148503d0ae771cbaa5f4e46bf7a428f6e525
richard@burnsides.org <richard@burnsides.org@d86039bd-945b-f367-b13c-a3cd50f3cf67>
2013-04-11 12:34:18 +0000

    git-svn-id: https://minuteman.googlecode.com/svn/trunk@141 d86039bd-945b-f367-b13c-a3cd50f3cf67

--- AVC_2013_Beetle/AVC_2013_Beetle.ino
+++ AVC_2013_Beetle/AVC_2013_Beetle.ino
@@ -75,17 +75,22 @@ LOW		NEUTRAL		HIGH
 #include <math.h>
 
 //Function and Object Declarations
-SoftwareSerial serial_bluetooth(3,2); // RX, TX
-SoftwareSerial serial_gps(4,5); // RX, TX
+SoftwareSerial serial_bluetooth(3,2);	// RX, TX
+SoftwareSerial serial_gps(4,5);			// RX, TX
 HMC5883L compass;
 TinyGPS gps;
+Servo steering;
+Servo speed;
+
+//Steering and Throttle
+#define SERVO 6					// pin # for steering servo
+#define THROTTLE 7				// pin # for throttle
 
 //GPS Variables
 float flat, flon;
 unsigned long age, date, time, chars;
 unsigned short sentences, failed;
-
-int print_time = 0;
+double waypoint_distance, waypoint_heading = 0;
 
 //Compass
 #define compass_x_cal -20.5
@@ -96,11 +101,36 @@ double compass_heading = 0;
 double angle_diff;				// for the compass
 
 
+//GPS Waypoints
+int waypoint_num = 0;
+const int waypoint_total = 15;	// <- should always be the same number of GPS waypoints
+// 0 = lat use, 1 = south lat use, 2 = north lat use
+double gps_array[15][3] = {{40.0651517950864528, -105.2097273131420, 0}, // 1st corner
+	{40.0650036900410714, -105.2097272812302, 1}, // 1st line up point
+	{40.0649066081190785, -105.2097109401257, 1}, // 2nd line up point or start of straight line
+	{40.06485, -105.20975, 1}, // just added today
+	{40.064528, -105.2097763511701, 1}, // lat point for start of turn
+	{40.06448686920148, -105.20985212357533, 0}, // 2nd corner, just inside of south parking lot
+	{40.06445080617122, -105.21016393037269, 0}, // bottom middle of south parking lot
+	{40.06449104008855, -105.21041203470733, 0}, // inbetween sidewalks for 3rd corner
+	{40.06450670940305, -105.2104463610966, 0}, // 3rd corner
+	{40.06465758628262, -105.21046433778312, 2}, // line up for inbetween next bottleneck
+	{40.06475817068338, -105.21046970220115, 2}, // past the bottleneck
+	{40.06493060073908, -105.21048043103721, 2}, // middle of the 3rd side
+	{40.06514305860027, -105.21048311324623, 2}, // end of 3rd side
+	{40.06519745591719, -105.21041069360282, 0}, // 4th corner
+	{40.06518985782108, -105.21001238556421, 0}}; // way past the finish line
+
+int header_line = 0;				// counter that reprints the serial header line
+
 void setup(){
 	serial_bluetooth.begin(115200);
 	serial_bluetooth.println("bluetooth initialized");
 	serial_gps.begin(9600);
 	serial_bluetooth.println("gps initialized");
+
+	steering.attach(SERVO);		//Servo Initialization
+	speed.attach(THROTTLE);		//Speed control initialization
 
 	//compass initialization
 	Wire.begin(); // Start the I2C interface.
@@ -109,27 +139,81 @@ void setup(){
 	Serial.println("Setting measurement mode to continous.");
 	compass.SetMeasurementMode(Measurement_Continuous); // Set the measurement mode to Continuous
 
-
-	
-	delay(500);
+	delay(1000);
 }
 
 void loop(){
 	compass_measurement();
 	gps_data();
 
+	waypoint();
+	set_turn();
+	set_speed();
+
 	serial_data_log();
 
-	if(print_time > 31){
+	if(header_line > 31){
+		serial_bluetooth.println("");
 		serial_bluetooth.println("XAxis\tYAxis\tHeading\t\tLat\t\t\tLon\t\t\t\tFailed");
 		serial_bluetooth.println("------------------------------------------------------------------");
-		print_time = 0;
+		header_line = 0;
 	}
-	print_time++;
+	header_line++;
 }
 
-//GPS data update
+/* these functions need work. consider incorporating XXX code ideas.*/
 
+void set_speed(void){		// test this and delete the delays after it works
+	if(waypoint_distance <= 20)		speed.write(112);
+	if(waypoint_distance > 20)		speed.write(150);
+
+	while(waypoint_num >= waypoint_total) speed.write(90);		// shuts off the vehicle by setting speed to 0
+
+	return;
+}
+
+void set_turn(void){				// Set servo to steer in the direction of the next waypoint
+	double servo_angle;
+
+	angle_diff = compass_heading - waypoint_heading;
+
+	if(angle_diff < -180)	angle_diff += 360.0;
+	if(angle_diff >= 180)	angle_diff -= 360.0;
+
+	servo_angle = angle_diff/2.0 + 90.0;		//changes domain from -180...180 to 0...180
+	servo_angle = 180.0 - servo_angle;
+	
+	// this section sets the servo/turning limits
+	if(servo_angle > 130.0)		steering.write(130);
+	else if(servo_angle < 50.0)	steering.write(50);
+	else						steering.write(servo_angle);
+	return;
+}
+
+void waypoint(void){				// Distance and angle to next waypoint
+	double x, y;
+
+	x = 69.1*(gps_array[waypoint_num][1] - flon) * cos(flat/57.3);
+	y = 69.1*(gps_array[waypoint_num][0] - flat);
+	
+	waypoint_distance = sqrt(pow(x,2) + pow(y,2))*5280.0;	// converts distance to feet
+	waypoint_heading = atan2(y,x)*180.0/M_PI;				// 180/pi converts from rads to degrees
+
+	if(waypoint_heading < 0)	waypoint_heading += 360.0;	// ensures heading is ALWAYS positive
+
+// this is the waypoint acceptance section. consider breaking it out into another function
+	if((gps_array[waypoint_num][2] == 1) && (gps_array[waypoint_num][0] > flat)) waypoint_num++; // going south
+	else if((gps_array[waypoint_num][2] == 2) && (gps_array[waypoint_num][0] < flat)) waypoint_num++; // going north
+	else if(waypoint_distance < 10) waypoint_num++;
+
+	return;
+}
+
+
+
+/*Working Functions That Are Working Well*/
+
+//GPS data update
 void gps_data(){
 	serial_gps.listen();
 	bool newdata = false;
@@ -152,7 +236,6 @@ void gpsdump(TinyGPS &gps){		// GPS Calculation
 	return;
 }
 
-
 void compass_measurement(){
 	MagnetometerRaw raw = compass.ReadRawAxis();	//get raw 
 	YAxis = raw.YAxis + compass_y_cal;	//adjust YAxis with calibration factor
@@ -166,8 +249,6 @@ void compass_measurement(){
 
 	compass_heading = compass_heading * 180.0/PI;	// Convert radians to degrees for readability.
 
-//	delay(100); //delay shouldn't be needed because of all the other stuff going on
-
 	return;
 }
 
@@ -179,5 +260,36 @@ void serial_data_log(){			// Serial Data Logging
 	serial_bluetooth.print(flon,8); serial_bluetooth.print("\t");
 	serial_bluetooth.println(failed);
 
+	// i should include waypoint heading, distance, angle diff, etc
+	// latitude_array = gps_array[waypoint_num][0]*1000000;
+	// longitude_array = gps_array[waypoint_num][1]*1000000;
+	// wp_distance = waypoint_distance;
+	// wp_heading = waypoint_heading;
+	// long angle_string;
+	// angle_string = angle * 360.0 / GYRO_CAL;
+
+	
+	
 	return;
 }
+
+/* GPS Routes
+
+SC - Parking Lot - old
+{{39.538298770540344,-105.01172583175334},
+{39.53849527942753,-105.01150857282313},
+{39.538635938078826,-105.01140396667155},
+{39.538669034190654,-105.01133154702815},
+{39.53855319773023,-105.01111160588893},
+{39.53844356661642,-105.01092921567592},
+{39.53837323713148,-105.0108541138235},
+{39.538269811288885,-105.01089702916774},
+{39.53810019257341,-105.0110874660078},
+{39.537924367857656,-105.01124303413066},
+{39.53787058609094,-105.01135568690928},
+{39.5379740125284,-105.01154344154033},
+{39.53809398700279,-105.0117365605894},
+{39.53815914891089,-105.01183747870347},
+{39.53820179479863,-105.01186061275624},
+{39.53826074760557,-105.01180830968045}};
+*/
