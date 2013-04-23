ef628629e8ffc5f58693491d4211b0b2cea39a4a
nathan.burnside@gmail.com <nathan.burnside@gmail.com@d86039bd-945b-f367-b13c-a3cd50f3cf67>
2013-04-23 02:33:36 +0000

    make for z axis only
    git-svn-id: https://minuteman.googlecode.com/svn/trunk@164 d86039bd-945b-f367-b13c-a3cd50f3cf67

--- Nathan/mpu_6050_example/mpu_6050_example.ino
+++ Nathan/mpu_6050_example/mpu_6050_example.ino
@@ -633,29 +633,11 @@ typedef union accel_t_gyro_union
 {
   struct
   {
-    uint8_t x_accel_h;
-    uint8_t x_accel_l;
-    uint8_t y_accel_h;
-    uint8_t y_accel_l;
-    uint8_t z_accel_h;
-    uint8_t z_accel_l;
-    uint8_t t_h;
-    uint8_t t_l;
-    uint8_t x_gyro_h;
-    uint8_t x_gyro_l;
-    uint8_t y_gyro_h;
-    uint8_t y_gyro_l;
     uint8_t z_gyro_h;
     uint8_t z_gyro_l;
   } reg;
   struct 
   {
-    int x_accel;
-    int y_accel;
-    int z_accel;
-    int temperature;
-    int x_gyro;
-    int y_gyro;
     int z_gyro;
   } value;
 };
@@ -721,7 +703,7 @@ void loop()
   // With the default settings of the MPU-6050,
   // there is no filter enabled, and the values
   // are not very stable.
-  error = MPU6050_read (MPU6050_ACCEL_XOUT_H, (uint8_t *) &accel_t_gyro, sizeof(accel_t_gyro));
+  error = MPU6050_read (MPU6050_GYRO_ZOUT_H, (uint8_t *) &accel_t_gyro, sizeof(accel_t_gyro));
   Serial.print(F("Read accel, temp and gyro, error = "));
   Serial.println(error,DEC);
 
@@ -733,46 +715,11 @@ void loop()
   uint8_t swap;
   #define SWAP(x,y) swap = x; x = y; y = swap
 
-  SWAP (accel_t_gyro.reg.x_accel_h, accel_t_gyro.reg.x_accel_l);
-  SWAP (accel_t_gyro.reg.y_accel_h, accel_t_gyro.reg.y_accel_l);
-  SWAP (accel_t_gyro.reg.z_accel_h, accel_t_gyro.reg.z_accel_l);
-  SWAP (accel_t_gyro.reg.t_h, accel_t_gyro.reg.t_l);
-  SWAP (accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
-  SWAP (accel_t_gyro.reg.y_gyro_h, accel_t_gyro.reg.y_gyro_l);
   SWAP (accel_t_gyro.reg.z_gyro_h, accel_t_gyro.reg.z_gyro_l);
-
-
-  // Print the raw acceleration values
-
-  Serial.print(F("accel x,y,z: "));
-  Serial.print(accel_t_gyro.value.x_accel, DEC);
-  Serial.print(F(", "));
-  Serial.print(accel_t_gyro.value.y_accel, DEC);
-  Serial.print(F(", "));
-  Serial.print(accel_t_gyro.value.z_accel, DEC);
-  Serial.println(F(""));
-
-
-  // The temperature sensor is -40 to +85 degrees Celsius.
-  // It is a signed integer.
-  // According to the datasheet: 
-  //   340 per degrees Celsius, -512 at 35 degrees.
-  // At 0 degrees: -512 - (340 * 35) = -12412
-
-  Serial.print(F("temperature: "));
-  dT = ( (double) accel_t_gyro.value.temperature + 12412.0) / 340.0;
-  Serial.print(dT, 3);
-  Serial.print(F(" degrees Celsius"));
-  Serial.println(F(""));
-
 
   // Print the raw gyro values.
 
   Serial.print(F("gyro x,y,z : "));
-  Serial.print(accel_t_gyro.value.x_gyro, DEC);
-  Serial.print(F(", "));
-  Serial.print(accel_t_gyro.value.y_gyro, DEC);
-  Serial.print(F(", "));
   Serial.print(accel_t_gyro.value.z_gyro, DEC);
   Serial.print(F(", "));
   Serial.println(F(""));
