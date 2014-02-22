81e4017312af3111c763522973247788237962c1
rkburnside <richard@burnsides.org>
2014-02-22 10:06:17 -0700

    --created multiple files for the code-- 1. GYRO.cpp added. it contains all of the gyro functions 2. DECLARATIONS.h added. it contains all of the necessary function declarations and some of the include files.
    *note: the arduino ide is honky. see: http://jamesreubenknowles.com/including-libraries-in-the-arduino-ide-1625
    Any Arduino libraries that are needed by the files outside of the sketch (.ino) file must also be listed in the sketch file itself.
    The sketch is parsed for include files. The sketch, all included header files, and the corresponding source files, are copied to another directory for compiling. From that directory, library-based include files are NOT available unless they are included in the sketch and copied to the build directory.
    
    **note: this code compiles, but has not been road/functionally tested.

--- AVC_2014/AVC_2014.ino
+++ AVC_2014/AVC_2014.ino
@@ -1,4 +1,4 @@
-/* Minuteman / Roadrunner competition code - just some changes.
+/* Minuteman / Roadrunner competition code - creation of the multi-file branch
 
 Pin Assignments:
 
@@ -28,39 +28,46 @@ STEERING AUTONOMOUS MODE - MOVE THROLLER INPUT TO 4 AND THROTTLE OUTPUT TO 4
 
 */
 
+/*to do
+x1. move the gyro to another file, make all values local, then static, then global
+2. move the servo functions to another file
+3. move the steering to another file
+4. move the menu to another file
+etc.
+*/
+
+#include "DECLARATIONS.h"
 #include <Servo.h>
 #include <EEPROM.h>
-#include "AVC_2014.h"
-#include "EEPROMAnything.h"
-#include "Wire.h"
-//#include <MemoryFree.h>
-
-// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
+#include <Wire.h>
 #include <I2Cdev.h>
-//#include "MPU6050_6Axis_MotionApps20.h"
 #include <MPU6050.h>
-//#include "new_gyro.h"
 
+/* arduino ide is honky...
+see: http://jamesreubenknowles.com/including-libraries-in-the-arduino-ide-1625
+Any Arduino libraries that are needed by the files outside of the sketch (.ino) file must also be listed in the sketch file itself.
+The sketch is parsed for include files. The sketch, all included header files, and the corresponding source files, are copied to another directory for compiling. From that directory, library-based include files are NOT available unless they are included in the sketch and copied to the build directory.
+*/
 
 //these are used for setting and clearing bits in special control registers on ATmega
+extern long accum; //this is ONLY used to reset the 0 the gyro angle for real (setting angle to 0 does nothing!!! (never forget last year's debacle))
+extern double angle;
 
-volatile boolean gyro_flag = false, cal_flag = false;
-boolean manual, automatic, aux=false, running=false, first=true;
+volatile boolean gyro_flag = false;
+bool manual, automatic, aux=false, running=false, first=true;
 volatile byte clicks = 0;
 unsigned int click_calibration_counter = 0;
-long gyro_count = 0, gyro_null=0, accum=0, time=0;
+long time=0;
 long count, proximity, previous_proximity=50;
 double x_wp = 0, y_wp = 0;
-double angle_diff, angle_last, angle_target, x=0, y=0, angle=0;
+double angle_diff, angle_last, angle_target, x=0, y=0;
 int steer_limm = 300, steer_us;
 long speed_cur=0, speed_new=0, speed_old=0;
 byte wpr_count=1, wpw_count=1;
-const byte InterruptPin = 2 ;		//intterupt on digital pin 2
+const byte InterruptPin = 2 ;		//interrupt on digital pin 2
 double cross_product=0, target_x=0, target_y=0;
 
 Servo steering, esc;
-MPU6050 accelgyro;
 
 struct position_structure {
 
@@ -381,198 +388,6 @@ void edit_waypoint(){
 	
 	Serial.println();
 	
-	return ;
-}
-
-void gyro_calibration(){
-	Serial.println();
-	setup_mpu6050();
-	calculate_null();
-	cal_flag = true;
-
-	Serial.println("calibrate gyro");
-	do{
-		read_FIFO();
-		
-		if((millis()-time)> 250){
-			Serial.println(accum);
-			time = millis();
-		}
-		get_mode();
-	} while(manual);
-	
-	cal_flag = false;
-	Serial.println();
-	return ;
-}
-
-void watch_angle(){
-	Serial.println();
-	setup_mpu6050();
-	calculate_null();
-
-	Serial.println("watch angle");
-	do {
-		read_FIFO();
-
-		if((millis()-time)> 250){
-			//Serial.println(angle);
-			Serial.println(angle*180.0/3.14159,5);
-			//Serial.println(accum);
-			time = millis();
-		}
-		get_mode();
-	} while(manual);		//keep summing unitil we turn the mode switch off.
-
-	return ;
-}
-
-void watch_gyro(){
-	Serial.println();
-	setup_mpu6050();
-	calculate_null();
-
-	Serial.println("watch gyro");
-	do {
-		read_FIFO();
-
-		if((millis()-time)> 250){
-			Serial.println(accum);
-			time = millis();
-		}
-		get_mode();
-	} while(manual);		//keep summing unitil we turn the mode switch off.
-
-	return ;
-}
-
-void setup_mpu6050(){
-    // initialize device
-    Serial.println("Initializing I2C devices...");
-    accelgyro.initialize();
-
-    // verify connection
-    Serial.println("Testing device connections...");
-    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-	
-    // reset device
-    Serial.println(F("\nResetting MPU6050..."));
-    accelgyro.reset();
-    delay(30); // wait after reset
-
-
-    // disable sleep mode
-    Serial.println(F("Disabling sleep mode..."));
-    accelgyro.setSleepEnabled(false);
-
-    // get X/Y/Z gyro offsets
-    Serial.println(F("Reading gyro offset values..."));
-    int8_t xgOffset = accelgyro.getXGyroOffset();
-    int8_t ygOffset = accelgyro.getYGyroOffset();
-    int8_t zgOffset = accelgyro.getZGyroOffset();
-    Serial.print(F("X gyro offset = "));
-    Serial.println(xgOffset);
-    Serial.print(F("Y gyro offset = "));
-    Serial.println(ygOffset);
-    Serial.print(F("Z gyro offset = "));
-    Serial.println(zgOffset);
-
-	Serial.println(F("Setting clock source to Z Gyro..."));
-	accelgyro.setClockSource(MPU6050_CLOCK_PLL_ZGYRO);
-
-	// Serial.println(F("Setting DMP and FIFO_OFLOW interrupts enabled..."));
-	// accelgyro.setIntEnabled(0x12);
-
-	 Serial.println(F("Setting sample rate to 200Hz..."));
-	 accelgyro.setRate(0); // 1khz / (1 + 4) = 200 Hz
-
-	// Serial.println(F("Setting external frame sync to TEMP_OUT_L[0]..."));
-	// accelgyro.setExternalFrameSync(MPU6050_EXT_SYNC_TEMP_OUT_L);
-
-	Serial.println(F("Setting DLPF bandwidth to 42Hz..."));
-	accelgyro.setDLPFMode(MPU6050_DLPF_BW_42);
-
-	Serial.println(F("Setting gyro sensitivity to +/- 250 deg/sec..."));
-	accelgyro.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
-
-	// Serial.println(F("Setting X/Y/Z gyro offsets to previous values..."));
-	// accelgyro.setXGyroOffset(xgOffset);
-	// accelgyro.setYGyroOffset(ygOffset);
-	// accelgyro.setZGyroOffset(61);
-
-	// Serial.println(F("Setting X/Y/Z gyro user offsets to zero..."));
-	// accelgyro.setXGyroOffsetUser(0);
-	// accelgyro.setYGyroOffsetUser(0);
-	//accelgyro.setZGyroOffsetUser(0);
-	//Serial.print(F("Z gyro offset = "));
-    //Serial.println(accelgyro.getZGyroOffset());
-
-	// Serial.println(F("Setting motion detection threshold to 2..."));
-	// accelgyro.setMotionDetectionThreshold(2);
-
-	// Serial.println(F("Setting zero-motion detection threshold to 156..."));
-	// accelgyro.setZeroMotionDetectionThreshold(156);
-
-	// Serial.println(F("Setting motion detection duration to 80..."));
-	// accelgyro.setMotionDetectionDuration(80);
-
-	// Serial.println(F("Setting zero-motion detection duration to 0..."));
-	// accelgyro.setZeroMotionDetectionDuration(0);
-
-	Serial.println(F("Resetting FIFO..."));
-	accelgyro.resetFIFO();
-
-	Serial.println(F("Enabling FIFO..."));
-	accelgyro.setFIFOEnabled(true);
-	accelgyro.setZGyroFIFOEnabled(true);
-	
-	return ;
-}
-
-void calculate_null(){
-	Serial.println("CALCULATING NULL");
-
-	cal_flag = true;		//tell ADC ISR that we are calibrating,
-	accum = 0;				//reset the angle. angle will act as accumulator for null calculation
-	gyro_null = 0;			//make sure to not subract any nulls here
-	gyro_count = 0;
-
-	while(gyro_count < 5000){
-		read_FIFO();
-		//delay(10);
-		//Serial.println(gyro_count);
-	}
-	gyro_null = accum/gyro_count + NULL_FF;	//calculate the null. the -30 is a fudge factor for 5000 pts.
-	cal_flag = false;		//stop calibration
-	accum = 0;
-	
-
-	//should print null here
-	Serial.print("Null: ");
-	Serial.println(gyro_null);
-	
-	return ;
-}
-
-void read_FIFO(){
-	uint8_t buffer[2];
-	long temp = 0;
-	int samplz = 0;
-
-	samplz = accelgyro.getFIFOCount() >> 1;
-	//Serial.println("FIFO_COUNTH : ");
-	//Serial.println(samplz,DEC);
-	for(int i=0; i < samplz; i++){
-		accelgyro.getFIFOBytes(buffer, 2);
-		temp = ((((int16_t)buffer[0]) << 8) | buffer[1]);
-		accum -= (temp * 10) + gyro_null;
-		gyro_count++;
-		
-		if((accum > GYRO_CAL) && (!cal_flag)) accum -= GYRO_CAL*2; //if we are calculating null, don't roll-over
-		if((accum < -GYRO_CAL) && (!cal_flag)) accum += GYRO_CAL*2;
-	}
-	angle = (float)accum/(float)GYRO_CAL * 3.14159;
-
 	return ;
 }
 
---
+++ AVC_2014/DECLARATIONS.h
@@ -0,0 +1,42 @@
+// functions declaration file. FUNCTIONS.cpp
+#ifndef __FUNCTION_DECLARATIONS__
+#define __FUNCTION_DECLARATIONS__
+
+//gyro functions
+void setup_mpu6050();
+void read_FIFO();
+void calculate_null();
+void gyro_calibration();
+void watch_angle();
+void watch_gyro();
+
+
+void get_mode();
+
+#endif
+
+
+#ifndef __INCLUDE_FILES__
+#define __INCLUDE_FILES__
+
+#include "AVC_2014.h"
+#include "EEPROMAnything.h"
+#include <I2Cdev.h> //declared 2x (1 here and 1 in the .ino)(shouldn't be, but arduino is dumb...see note below)
+#include <MPU6050.h> //declared 2x (1 here and 1 in the .ino)(shouldn't be, but arduino is dumb...see note below)
+
+//#include <MemoryFree.h>
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+
+//#include "MPU6050_6Axis_MotionApps20.h"
+
+//#include "new_gyro.h"
+
+/* arduino ide is stupid...
+see: http://jamesreubenknowles.com/including-libraries-in-the-arduino-ide-1625
+Any Arduino libraries that are needed by the files outside of the sketch (.ino) file must also be listed in the sketch file itself.
+The sketch is parsed for include files. The sketch, all included header files, and the corresponding source files, are copied to another directory for compiling. From that directory, library-based include files are NOT available unless they are included in the sketch and copied to the build directory.
+*/
+
+#endif
