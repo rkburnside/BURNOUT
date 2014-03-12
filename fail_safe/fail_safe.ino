352d1b99874a40b44761b07a9980cffc98018268
unknown <rburnsid@SSCLB2146NT2.acct02.us.lmco.com>
2014-03-12 12:20:19 -0600

    just a few more updates. the code is still broken, but it's getting closer.

--- fail_safe/fail_safe.ino
+++ fail_safe/fail_safe.ino
@@ -3,10 +3,30 @@ This file is intended for the arduino pro-mini to monitor channel 3 from the rad
 This code is based on the arduipilot fail safe developed by Chris Anderon, Jordi Munoz, Nathan (SparkFun).
 */
 
-#define SWITCH_STATE_MANUAL 0		//full manual control of the car
-#define SWITCH_STATE_1 1			//switch state 1
-#define SWITCH_STATE_2 2			//switch state 2
-#define SWITCH_STATE_AUTOMATIC 3	//full autonomous mode
+/* required functions
+
+1. function that times how quickly channel three is toggled from low to high back to low. When it has reached three times, it will hard reset the MCU
+
+2. function that will flash the LED in 1 of 4 ways:
+	manual = 0
+	state 1 = quick flashing
+	state 2 = slow flashing
+	automatic = constanst LED
+
+x3. function that will determine the state of channel 3. servo pulse range = 1mSec ~ 2mSec. 1mSec would cause the shaft to revolve fully left. A 2mSec positive pulse width would cause the drive shaft to revolve fully right. 1.5mSec pulse width would cause the shaft to turn to the middle of the revolution area.
+	
+	manual -> manual <1250ms
+	state 1 -> 1250ms <= state 1 < 1500ms
+	state 2 -> 1500ms <= state 2 < 1750ms
+	automatic -> 1750ms <= automatic
+
+*/
+
+
+#define SWITCH_POSITION_MANUAL 0		//full manual control of the car
+#define SWITCH_POSITION_1 1			//switch state 1
+#define SWITCH_POSITION_2 2			//switch state 2
+#define SWITCH_POSITION_AUTOMATIC 3	//full autonomous mode
 
 int rest_pin = 1;		//used to reset the external MCU
 int ch_3_in = 2;		//pin to monitor radio channel 3
@@ -15,6 +35,7 @@ int ch_3_state_2 = 4;		//output line 2 to external MCU
 int multiplexor = 5;	//multiplexor toggle
 int state_led = 13;		//MCU state led
 int pulse_length = 0;
+int switch_position = 0;
 
 void setup(){
 	pinMode(rest_pin, OUTPUT);
@@ -27,50 +48,50 @@ void setup(){
 	digitalWrite(rest_pin, LOW);
     digitalWrite(multiplexor, LOW);
 
-	state(SWITCH_STATE_MANUAL);
-	visual_status(SWITCH_STATE_MANUAL);
+	set_vehile_state(SWITCH_POSITION_MANUAL);
+	visual_status(SWITCH_POSITION_MANUAL);
 }
 
 void loop(){
-	pulse_length = pulseIn(ch_3_in, HIGH, 50000);
-	switch_position(pulse_length);
-	set_state(
+	pulse_length = pulseIn(ch_3_in, HIGH, 50000);	//function will time out after 50000 uS if no pulse is received
+	determine_switch_position(void);
+	set_vehile_state(void);
 }
 
-int switch_position(int pulse){
-	if 
-
+void determine_switch_position(void){
+	if(pulse_length < 1250) switch_position = SWITCH_POSITION_MANUAL;
+	else if((pulse_length >= 1250) && (pulse_length < 1500)) switch_position = SWITCH_POSITION_1;
+	else if((pulse_length >= 1500) && (pulse_length < 1750)) switch_position = SWITCH_POSITION_2;
+	else switch_position = SWITCH_POSITION_AUTOMATIC;
+	
+	return;
 }
 
-
-void state(int switch_status){
-	switch (switch_status){
-		case SWITCH_STATE_1:
+void set_vehile_state(void){
+	switch (switch_position){
+		case SWITCH_POSITION_1:
 			digitalWrite(multiplexor, LOW);
 			digitalWrite(ch_3_state_1, LOW);
 			digitalWrite(ch_3_state_2, LOW);
 			digitalWrite(multiplexor, HIGH);
 			break;
 
-		case SWITCH_STATE_2:
+		case SWITCH_POSITION_2:
 			digitalWrite(multiplexor, LOW);
 			digitalWrite(ch_3_state_1, LOW);
 			digitalWrite(ch_3_state_2, HIGH);
 			digitalWrite(multiplexor, HIGH);
 			break;
 		
-		case SWITCH_STATE_AUTOMATIC:
+		case SWITCH_POSITION_AUTOMATIC:
 			digitalWrite(multiplexor, LOW);
 			digitalWrite(ch_3_state_1, HIGH);
 			digitalWrite(ch_3_state_2, LOW);
 			digitalWrite(multiplexor, HIGH);
 			break;
 		
-		default:	//default state is SWITCH_STATE_MANUAL
+		default:	//default state is SWITCH_POSITION_MANUAL
 			digitalWrite(multiplexor, LOW);
-			digitalWrite(ch_3_state_1, HIGH);
-			digitalWrite(ch_3_state_2, HIGH);
-			digitalWrite(multiplexor, HIGH);
 	}
 	
 	return;
